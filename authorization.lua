d2aeab91afad2442175dd802c21845bc5e4d9700
Vlad <130040958+BlackCatUA1@users.noreply.github.com>
2023-04-13 20:13:14 +0300

    Create authorization.lua

---
+++ authorization.lua
@@ -0,0 +1,28 @@
+while true do
+term.clear()
+term.setCursorPos(1, 1)
+l = "Ваш_Логин"
+print("Enter your login: ")
+log = read()
+if log == l then
+  print("OK")
+  sleep(2)
+else
+  print("Error, please try again.")
+  sleep(2)
+end
+term.clear()
+term.setCursorPos(1, 1)
+p = "Ваш_Пароль"
+print("Enter your password: ")
+pass = read("*")
+if pass == p then
+  print("Access granted")
+  rs.setOutput("left", true)
+  sleep(3)
+  rs.setOutput("left", false)
+else
+  print("Access denied")
+  sleep(1)
+end
+end
