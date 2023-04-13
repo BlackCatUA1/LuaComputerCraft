0db3f095aa34341dbf80d384acf68b9b1dd58eb6
Vlad <130040958+BlackCatUA1@users.noreply.github.com>
2023-04-13 20:25:52 +0300

    Create psevdovirus.lua

---
+++ psevdovirus.lua
@@ -0,0 +1,7 @@
+os.pullEvent = os.pullEventRaw (Не позволяє виключити програму утримуючи CTRL+t)
+
+term.clear() (Очищає екран)
+print("THIS DEVICE HAS BEEN BLOCKED") (Виводить повідомлення на екран)
+while true do (Бескінечний цикл)
+sleep(100) ("Сон" компютера на 100 секунд)
+end
